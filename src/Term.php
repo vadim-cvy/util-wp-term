0b35b0b03276138b694d40a4373c727a443e1c6f
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-09-22 18:13:53 +0300

    Fix taxonomy slug retrieving

--- src/Term.php
+++ src/Term.php
@@ -2,29 +2,20 @@
 namespace Cvy\WP\Term;
 
 use \Cvy\WP\PostsQuery\PostsQuery;
-use \Cvy\WP\PostsQuery\PostsQueryArgs;
 
-abstract class Term extends \Cvy\WP\Object\WPObject
+class Term extends \Cvy\WP\Object\WPObject
 {
-  protected $taxonomy_slug;
-
-  public function __construct( int $id, string $taxonomy_slug = null )
-  {
-    parent::__construct( $id );
-
-    $this->taxonomy_slug = $taxonomy_slug;
-  }
-
+  /**
+   * @return string Parent taxonomy slug.
+   */
   public function get_taxonomy_slug() : string
   {
-    if ( ! $this->taxonomy_slug )
-    {
-      throw new \Exception( 'Taxonomy slug is not set!' );
-    }
-
-    return $this->taxonomy_slug;
+    return $this->get_original()->taxonomy;
   }
 
+  /**
+   * @return string Term name.
+   */
   public function get_label() : string
   {
     return $this->get_original()->name;
@@ -35,7 +26,12 @@ abstract class Term extends \Cvy\WP\Object\WPObject
     return $this->get_taxonomy_slug() . '_' . $this->get_id();
   }
 
-  public function get_original() : object
+  /**
+   * WP original term instance.
+   *
+   * @return \WP_Term
+   */
+  public function get_original() : \WP_Term
   {
     return get_term( $this->get_id(), $this->get_taxonomy_slug() );
   }
@@ -55,7 +51,7 @@ abstract class Term extends \Cvy\WP\Object\WPObject
     delete_term_meta( $this->get_id(), $selector );
   }
 
-  public function get_posts( PostsQuery $posts_query ) : array
+  public function get_posts( PostsQuery $posts_query = null ) : array
   {
     $posts_query->patch([
       'tax_query' => [[
